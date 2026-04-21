a9f99db00f4e2113a323238306accade1b710da7
Ishaan-Gill <ishaangill@gmail.com>
2026-04-21 17:26:19 +0530

    text-to-sql agent (basic) complete

--- app/actions.ts
+++ app/actions.ts
@@ -1,26 +1,54 @@
-"user server"
+"use server"
 
-import { ChatWatsonx } from "@langchain/community/chat_models/ibm";
+import { ChatGoogle } from "@langchain/google";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
 import {
     mapStoredMessagesToChatMessages,
     StoredMessage
-} from "@langchain/core/messages";
-import { version } from "os";
+} from "@langchain/core/messages"
+import { customerTable, orderTable } from "./constants";
+import { execute } from "./database";
 
 export async function message(messages: StoredMessage[]) {
     const deserialized = mapStoredMessagesToChatMessages(messages);
 
+    const getFromDB = tool(
+        async (input) => {
+            if (input?.sql) {
+                const result = await execute(input.sql);
+                console.log({ result, sql: input.sql });
+
+                return JSON.stringify(result);
+            }
+            return null;
+        },
+        {
+            name: "get_from_db",
+            description: `Get data from a database, the database has the following schema:
+  
+      ${orderTable}
+      ${customerTable}  
+      `,
+            schema: z.object({
+                sql: z
+                    .string()
+                    .describe(
+                        "SQL query to get data from a SQL database. Always put quotes around the field and table arguments."
+                    ),
+            }),
+        }
+    )
+
+
+
     const agent = createReactAgent({
-        llm: new ChatWatsonx({
-            model: "mistrallai/mistral-large",
-            projectId: process.env.WATSONX_AI_PROJECT_ID,
-            serviceUrl: process.env.WATSONX_AI_ENDPOINT,
-            version: "2026-04-13",
+        llm: new ChatGoogle({
+            model: "gemini-2.5-flash",
+            apiKey: process.env.GOOGLE_API_KEY,
         }),
-        tools: [],
+        tools: [getFromDB],
     });
 
     const response = await agent.invoke({
